7dcc38fce9535485ae598c3049e0909befc069ad
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2020-02-27 05:33:49 -0600

    Finish the subprocess control part of reset function

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Kubernetes cluster simulator for evaluating schedulers in reinforcement learning
 ## TODO
 
 1.  Improve the remote scheduler interface
+2.  Set up viper config path so viper can get the correct config file
---
+++ subProcessTest/subProcessTest.py
@@ -0,0 +1,31 @@
+import os
+import shlex
+import signal
+import subprocess
+import time
+
+# cmd = 'go run ../remoteScheTest/*.go --config=../remoteScheTest/config'
+# args = shlex.split(cmd)
+
+child = subprocess.Popen('go run ../remoteScheTest/*.go --config=../remoteScheTest/config', shell=True, start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+# child = subprocess.Popen(args, start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+print("Tester pid: ", end = '')
+print(child.pid)
+
+print("Tester code: ", end = '')
+print(child.poll())
+
+for i in range(10):
+    print(i)
+    time.sleep(1)
+
+print("Kill tester")
+os.killpg(child.pid, signal.SIGTERM)
+# child.terminate()
+
+child.wait()
+
+print("Tester code: ", end = '')
+
+print(child.poll())
